f89e60171f50454dd67dd72b828d302b5a579372
xyushi <ayuship03@gmail.com>
2022-07-06 21:20:12 +0530

    Commit Message

---
+++ dice.js
@@ -0,0 +1,31 @@
+var player1 = 0
+var player2 = 0
+
+var turn = 1
+
+var target = 30
+
+function roll() {
+    var num = getRndInteger(1, 6);
+    document.getElementById('val3').innerText = ' ' + num;
+    if (turn === 1) {
+        player1 = player1 + num;
+        turn = 2;
+        document.getElementById('val1').innerText = ' ' + player1;
+        if (player1 >= target) {
+            document.getElementById('val4').innerText = 'Player 1 Wins!';
+        }
+    }
+    else if (turn === 2) {
+        player2 = player2 + num;
+        turn = 1;
+        document.getElementById('val2').innerText = ' ' + player2;
+        if (player2 >= target) {
+            document.getElementById('val4').innerText = 'Player 2 Wins!';
+        }
+    }
+}
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
